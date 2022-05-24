06a7d7386fe2482baa48ea08d4f212d83497ef67
Vero0102 <104280568+Vero0102@users.noreply.github.com>
2022-05-25 01:19:05 +0300

    Домашнее задание выполнено.

--- src/pro/sky/java/course1/Main.java
+++ src/pro/sky/java/course1/Main.java
@@ -3,9 +3,39 @@ package pro.sky.java.course1;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Выполняем домашнее задание");
-        System.out.println("Делаем комит и пуш");
-        System.out.println("Переходим в гитхаб");
+        //Задача №1
+        int i = 1;
+        int a = 10;
+
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+
+        System.out.println();
+
+        for (a = 10; a > 0; a = a - 1) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+
+        //Задача №2
+
+        int friday = 6;
+        for (friday = 6; friday <= 31; friday = friday + 7) {
+            System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет.");
+        }
+
+        //Задача №3
+
+        int currentYear = 2022;
+        int yearOfComet = 0;
+        for (yearOfComet = currentYear - 200; yearOfComet <= 2122; yearOfComet = yearOfComet + 1) {
+            if (yearOfComet % 79 == 0) {
+                System.out.println(yearOfComet);
+            }
+        }
+
 
     }
 }
