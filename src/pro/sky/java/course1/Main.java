8f73b1a77666baa84ddd0bd90f2af640a86181a0
Vero0102 <104280568+Vero0102@users.noreply.github.com>
2022-05-24 22:41:14 +0300

    Homework

--- src/pro/sky/java/course1/Main.java
+++ src/pro/sky/java/course1/Main.java
@@ -3,6 +3,9 @@ package pro.sky.java.course1;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Выполняем домашнее задание");
+        System.out.println("Делаем комит и пуш");
+        System.out.println("Переходим в гитхаб");
+
     }
 }
